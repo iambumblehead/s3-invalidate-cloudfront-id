4c2c00eb5270ec1eb5c9244cb3f32dadfb58431c
chris <chris@bumblehead.com>
2021-12-17 18:18:11 -0800

    require REGION param, increment to 1.0.0 version

--- s3-invalidate-cloudfront-id.mjs
+++ s3-invalidate-cloudfront-id.mjs
@@ -5,6 +5,7 @@ import {
 
 // option names to local names
 const ENV = process.env;
+const ENV_REGION = 'S3_INVALIDATE_CLOUDFRONT_REGION';
 const ENV_ACCESS_KEY_ID = 'S3_INVALIDATE_CLOUDFRONT_ACCESS_KEY_ID';
 const ENV_SECRET_ACCESS_KEY = 'S3_INVALIDATE_CLOUDFRONT_SECRET_ACCESS_KEY';
 const ENV_CLOUDFRONT_ID = 'S3_INVALIDATE_CLOUDFRONT_ID';
@@ -25,13 +26,15 @@ const invalidateCache = async () => {
 
   const [
     AWS_CLOUDFRONT_ID,
+    AWS_REGION,
     AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY
-  ] = envGet(ENV_CLOUDFRONT_ID, ENV_ACCESS_KEY_ID, ENV_SECRET_ACCESS_KEY);
+  ] = envGet(
+    ENV_CLOUDFRONT_ID, ENV_REGION, ENV_ACCESS_KEY_ID, ENV_SECRET_ACCESS_KEY);
 
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudFront.html
   const cloudfront = new CloudFrontClient({
-    region : 'eu-central-1',
+    region : AWS_REGION,
     credentials : {
       accessKeyId : AWS_ACCESS_KEY_ID,
       secretAccessKey : AWS_SECRET_ACCESS_KEY
