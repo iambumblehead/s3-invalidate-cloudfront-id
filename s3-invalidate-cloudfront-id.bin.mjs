c1c0edd1e423109c04a5a328a5d81471828970d6
chris <chris@bumblehead.com>
2022-01-04 13:00:51 -0800

    update bin file to import and call main cloudfront default function

--- s3-invalidate-cloudfront-id.bin.mjs
+++ s3-invalidate-cloudfront-id.bin.mjs
@@ -1,2 +1,15 @@
 #! /usr/bin/env node
-import './s3-invalidate-cloudfront-id.mjs';
+import invalidateCloudfrontId from './s3-invalidate-cloudfront-id.mjs';
+
+try {
+  const res = invaludateCloudfrontId( process.env );
+
+  console.log('[...] invalidation:', JSON.stringify(res, null, '  '));
+} catch (e) {
+  console.error('Cloudfront Error!!');
+  console.error(`Code: ${e.code}`);
+  console.error(`Message: ${e.message}`);
+  console.error(`AWS Request ID: ${e.requestId}`);
+
+  throw new Error('Cloudfront invalidation failed!');
+}
